0d262965c58295a1ed6878646544a8e1178df5c2
Amano Spica <hebimeme@gmail.com>
2024-05-02 19:34:52 +0900

    Curry-Botと統合

---
+++ app/curry_bot/curry_bot.py
@@ -0,0 +1,121 @@
+import os
+import json
+import datetime
+
+import discord
+import dotenv
+
+from logger import Logger
+
+logger = Logger()
+dotenv.load_dotenv()
+
+TOKEN = os.environ.get("Cu_TOKEN")
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+client = discord.Client(intents=intents)
+
+VC_channel = int(os.environ.get("Cu_Voice_Channel"))
+Text_channel = int(os.environ.get("Cu_Text_Channel"))
+
+data_json_path = "app/curry_bot/data.json"
+
+async def save_data(data):
+    with open(data_json_path, "w", encoding="UTF-8") as f:
+        json.dump(data, f, indent=4)
+
+async def load_data():
+    with open(data_json_path, "r", encoding="UTF-8") as f:
+        return json.load(f)
+
+def timedelta_to_string(td: datetime.timedelta) -> str:
+    seconds = int(td.total_seconds())
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    if td.days > 0:
+        hours += td.days * 24
+    return f"{hours:02d}時間{minutes:02d}分{seconds:02d}秒"
+
+def cu_main():
+    @client.event
+    async def on_ready():
+        logger.info(f'We have logged in as {client.user}')
+    
+    @client.event
+    async def on_voice_state_update(member, before, after):
+        if before.channel != after.channel and not member.bot:
+            if before.channel is None and after.channel.id == VC_channel:
+                members = [member for member in after.channel.members if not member.bot]
+                if len(members) == 1:
+                    channel = client.get_channel(Text_channel)
+                    logger.info(f"{member.name} が通話を開始しました")
+                    data = await load_data()
+                    started_time = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+                    data["Started_time"] = started_time
+                    await save_data(data)
+                    if member.nick is not None:
+                        name = member.nick
+                    else:
+                        name = member.name
+                    embed = discord.Embed(
+                        title=f"{name} が通話を開始しました",
+                        color=0x4169e1
+                    )
+                    await channel.send(embed=embed)
+    
+                logger.info(f"{member.name} が参加しました。")
+    
+            elif after.channel is None and before.channel.id == VC_channel:
+                members = [member for member in before.channel.members if not member.bot]
+                logger.info(f"{member.name} が退出しました。")
+                if len(members) == 0:
+                    channel = client.get_channel(Text_channel)
+                    data = await load_data()
+                    if "Started_time" in data and not data["Started_time"] == "None":
+                        started_time = datetime.datetime.strptime(data["Started_time"], "%Y/%m/%d %H:%M:%S")
+                        now_time = datetime.datetime.now()
+                        talking_time: str = timedelta_to_string(now_time - started_time)
+                        data["Started_time"] = "None"
+                        await save_data(data)
+                        logger.info(f"{member.name} が通話を終了しました。")
+                        if member.nick is not None:
+                            name = member.nick
+                        else:
+                            name = member.name
+                        logger.info(f"通話時間：{talking_time}")
+                        embed = discord.Embed(
+                            title=f"{name} が通話を終了しました",
+                            description=f"通話時間：{talking_time}\n通話開始時刻：{started_time.strftime('%Y/%m/%d %H:%M:%S')}\n通話終了時刻：{now_time.strftime('%Y/%m/%d %H:%M:%S')}",
+                            color=0xff1493
+                        )
+                        await channel.send(embed=embed)
+    
+                    else:
+                        logger.info(f"{member.name} が通話を終了しました。")
+                        if member.nick is not None:
+                            name = member.nick
+                        else:
+                            name = member.name
+                        embed = discord.Embed(
+                            title=f"{name} が通話を終了しました",
+                            description="--  通話開始データが存在しません  --",
+                            color=0xff1493
+                        )
+                        await channel.send(embed=embed)
+    
+    
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+    
+        if message.content.startswith('$curry'):
+            embed = discord.Embed(
+                title="Curry Server BOT",
+                description="I am Curry Server BOT.\nI love Curry and Rice very much.",
+                color=0xbe7a11
+            )
+            await message.channel.send(embed=embed)
+    
+    client.run(TOKEN)
--- app/server.py
+++ app/server.py
@@ -15,7 +15,3 @@ async def head_root():
 
 def start():
 	uvicorn.run(app, host="0.0.0.0", port=8080)
-
-def server_thread():
-	t = Thread(target=start)
-	t.start()
